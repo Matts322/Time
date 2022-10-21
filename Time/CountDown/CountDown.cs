266b16f689e3d1f02daaedbd23724c03dcd9d3f8
Sinyakov <Sinyakov@DESKTOP-P08O7QJ>
2022-10-21 12:46:59 +0300

    splt

---
+++ Time/CountDown/CountDown.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
+
+namespace Time
+{
+    public partial class MainWindow
+    {
+        private readonly DispatcherTimer timerCountDown = new DispatcherTimer();
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            //if (CountdownTime.Text != "0")
+            if (!string.Equals(CountdownTime.Text, "0"))
+            {
+                CountdownTime.Text = (int.Parse(CountdownTime.Text) - 1).ToString();
+            }
+            else
+            {
+                timerCountDown.Stop();
+                timerCountDown.Tick -= Timer2_Tick;
+                ResetButton.IsEnabled = true;
+            }
+
+        }
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(CountdownTime.Text) || !int.TryParse(CountdownTime.Text, out int a) || a < 0)
+            {
+                return;
+            }
+
+            ResetButton.IsEnabled = false;
+            timerCountDown.Tick += new EventHandler(Timer2_Tick);
+            timerCountDown.Interval = new TimeSpan(0, 0, 1);
+            timerCountDown.Start();
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            timerCountDown.Stop();
+            timerCountDown.Tick -= Timer2_Tick;
+            ResetButton.IsEnabled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CountdownTime.Text = string.Empty;
+        }
+    }
+}
