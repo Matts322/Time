0d7f959f5151c379897bc97c85768025c735d28d
user <user@WIN-I9I69UKK4IF>
2022-10-23 15:22:49 +0300

    add enter press

--- Time/MainWindow.xaml.cs
+++ Time/MainWindow.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Windows;
-using System.Windows.Threading;
-using System.Net;
-using System.Text;
-using Newtonsoft.Json;
-using System.Threading.Tasks;
-using System.Threading;
+﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Time
 {
@@ -15,13 +8,10 @@ namespace Time
     /// </summary>
     public partial class MainWindow : Window
     {
-        
         public MainWindow()
         {
             InitializeComponent();
             InitTime();
         }
-
-        
     }
 }
